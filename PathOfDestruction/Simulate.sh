ae844c3663f614d1f1cc53a257a35cbf63614ea0
Tiger Sachse <tiger@sachse.io>
2018-08-15 00:05:05 -0400

    updated pdf

--- PathOfDestruction/Simulate.sh
+++ PathOfDestruction/Simulate.sh
@@ -1,6 +1,8 @@
 # Automatically load a prewritten solution to test the
 # testing and grading scripts.
+# Written by Tiger Sachse.
 
+# Run these lines to use the Test.sh script.
 if [ "$1" == "--test" ]
 then
     cp ../Solutions/PathOfDestruction.java .
@@ -8,6 +10,7 @@ then
     rm PathOfDestruction.java
 fi
 
+# Run these lines for the Grade.sh script.
 if [ "$1" == "--grade" ]
 then
     cp ../Solutions/PathOfDestruction.java PathOfDestruction-25.java
